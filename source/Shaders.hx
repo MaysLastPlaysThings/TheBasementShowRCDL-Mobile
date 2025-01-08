a01ab3c04a63c1d9075b2595634ec430b06b5df4
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-01-08 21:01:35 +0200

    ig?

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -63,25 +63,25 @@ class OldTVShader extends FlxShader
 
     float onOff(float a, float b, float c)
     {
-    	return step(c, sin(iTime + a*cos(iTime*b)));
+    	return step(c, sin(iTime + a * cos(iTime * b)));
     }
 
     float ramp(float y, float start, float end)
     {
     	float inside = step(start,y) - step(end,y);
-    	float fact = (y-start)/(end-start)*inside;
-    	return (1.-fact) * inside;
+    	float fact = (y - start)/(end-start) * inside;
+    	return (1.0 - fact) * inside;
 
     }
 
     vec4 getVideo(vec2 uv)
       {
       	vec2 look = uv;
-        	float window = 1./(1.+20.*(look.y-mod(iTime/4.,1.))*(look.y-mod(iTime/4.,15.)));
-        	look.x = look.x + (sin(look.y*10. + iTime)/540.*onOff(4.,4.,.3)*(1.+cos(iTime*80.))*window)*(glitchModifier*2.);
-        	float vShift = 0.4*onOff(2.,3.,.9)*(sin(iTime)*sin(iTime*20.) +
-        										 (1 + 0.1*sin(iTime*200.)*cos(iTime)));
-        	look.y = mod(look.y + vShift*glitchModifier, 1.);
+        	float window = 1.0/(1.+20.0 * (look.y - mod(iTime/4.0, 1.0)) * (look.y - mod(iTime/4.0, 15.0)));
+        	look.x = look.x + (sin(look.y * 10.0 + iTime)/540.0 * onOff(4.0, 4.0,0.3) * (1.0 + cos(iTime * 80.0)) * window) * (glitchModifier * 2.0);
+        	float vShift = 0.4 * onOff(2.0, 3.0, 0.9) * (sin(iTime) * sin(iTime*20.) +
+        										 (1 + 0.1 * sin(iTime * 200.0) * cos(iTime)));
+        	look.y = mod(look.y + vShift * glitchModifier, 1.0);
       	vec4 video = flixel_texture2D(bitmap,look);
 
       	return video;
@@ -94,7 +94,7 @@ class OldTVShader extends FlxShader
       	uv.x *= 1.0 + pow((abs(uv.y) / 4.5), 2);
       	uv.y *= 1.0 + pow((abs(uv.x) / 3.5), 2);
       	uv  = (uv / 2.0) + 0.5;
-      	uv =  uv *0.92 + 0.04;
+      	uv =  uv * 0.92 + 0.04;
       	return uv;
     	return uv;
     }
@@ -112,16 +112,14 @@ class OldTVShader extends FlxShader
         vec2 f = fract(uv);
 
         float a = random(i);
-        float b = random(i + vec2(1.,0.));
-    	float c = random(i + vec2(0., 1.));
-        float d = random(i + vec2(1.));
+        float b = random(i + vec2(1.0, 0.0));
+    	float c = random(i + vec2(0.0, 1.0));
+        float d = random(i + vec2(1.0));
 
-        vec2 u = smoothstep(0., 1., f);
+        vec2 u = smoothstep(0.0, 1.0, f);
 
-        return mix(a,b, u.x) + (c - a) * u.y * (1. - u.x) + (d - b) * u.x * u.y;
-
+        return mix(a,b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
     }
-
 
     vec2 scandistort(vec2 uv) {
     	float scan1 = clamp(cos(uv.y * 3.0 + iTime), 4.0, 1.0);
@@ -144,19 +142,18 @@ class OldTVShader extends FlxShader
       float grainFactor = filmGrainNoise(iTime, uv);
 
 
-      video.r = getVideo(vec2(x+uv.x+0.001,uv.y+1)).x + abs(sin(0.12 * redOpac)); // used for sirokous fire part
-      video.g = getVideo(vec2(x+uv.x-0.001,uv.y+1)).y + abs(sin(0.06 * blueOpac));
-      video.b = getVideo(vec2(x+uv.x-0.001,uv.y+1)).z + abs(sin(0.06 * blueOpac));
-    	vigAmt = 2.+.1*sin(iTime + 5.*cos(iTime*5.));
+      video.r = getVideo(vec2(x + uv.x + 0.001, uv.y + 1)).x + abs(sin(0.12 * redOpac)); // used for sirokous fire part
+      video.g = getVideo(vec2(x + uv.x - 0.001, uv.y + 1)).y + abs(sin(0.06 * blueOpac));
+      video.b = getVideo(vec2(x + uv.x - 0.001, uv.y + 1)).z + abs(sin(0.06 * blueOpac));
+    	vigAmt = 2.0 + 0.1 * sin(iTime + 5.0  * cos(iTime * 5.0));
 
-    	float vignette = (1.1-vigAmt*(uv.y-.5)*(uv.y-.5))*(0.1-vigAmt*(uv.x-.5)*(uv.x-.5));
+    	float vignette = (1.1 - vigAmt * (uv.y- 0.5) * (uv.y - 0.5)) * (0.1 - vigAmt * (uv.x - 0.5) * (uv.x - 0.5));
 
-      gl_FragColor = mix(video,vec4(noise(uv * 75.)),.05);
+      gl_FragColor = mix(video,vec4(noise(uv * 75.0)), 0.05);
 
-      if(curUV.x<0. || curUV.x>1. || curUV.y<0. || curUV.y>1.){
+      if(curUV.x < 0.0 || curUV.x > 1.0 || curUV.y < 0.0 || curUV.y > 1.0) {
         gl_FragColor = vec4(0,0,0,0);
       }
-
     }
     ')
 
